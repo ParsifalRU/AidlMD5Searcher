6203d67df826e2711dae769f30dc2c1adb99d51d
ParsifalRU <bunin.vania2015@yandex.ru>
2023-04-30 21:58:02 +0500

    all_work_need_to_tp_on_aidl

--- app/src/main/java/com/example/aidlmd5searcher/SearchService.kt
+++ app/src/main/java/com/example/aidlmd5searcher/SearchService.kt
@@ -4,23 +4,43 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadPoolExecutor
 
 class SearchService : Service() {
 
     val searchList = arrayListOf<String>()
     val readyList = arrayListOf<String>()
 
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("LOGTAG", "Service Created")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d("LOGTAG", "onStartCommand")
+        onBind(intent)
+        return super.onStartCommand(intent, flags, startId)
+    }
+
     override fun onBind(intent: Intent): IBinder? {
         Log.d("LOGTAG", "Service - Start")
         val data = intent.getStringExtra("data")
         val searchHash = intent.getStringExtra("searchHash")
-        val thread = Thread {
+
+        val threadPoolExecutor = Executors.newFixedThreadPool(2) as ThreadPoolExecutor
+
+        threadPoolExecutor.execute {
+            comparator(data!!, searchHash!!)
+        }
+       /* val thread = Thread {
             //End or have hash
             comparator(data!!, searchHash!!)
         }
         if (!thread.isAlive){
                 thread.start()
-        }
+        }*/
         return null
     }
 
@@ -30,16 +50,19 @@ class SearchService : Service() {
         Log.d("LOGTAG", "слово - \n$hash")
         val numb = listAll.size
         for (i in 1 .. numb){
-            if(listAll[i].contains(hash)){
+            if(listAll[i-1].contains(hash)){
                 Log.d("LOGTAG", "найдено TRUE")
                 sendBroadcast(Intent("TRUE"))
                 readyList.add(hash)
+                stopSelf()
+                break
             }else{
                 Log.d("LOGTAG", "не найдено False")
                 sendBroadcast(Intent("FALSE"))
                 searchList.add(hash)
             }
         }
+        stopSelf()
     }
 
     override fun onDestroy() {
